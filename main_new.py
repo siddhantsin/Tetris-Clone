40bd845934bf6d33ed120cbc1e1caeb93527faef
demolix <singhsiddhant2013@gmail.com>
2019-10-19 13:40:47 -0400

    Updates to controls

--- main_new.py
+++ main_new.py
@@ -2,6 +2,7 @@ import pygame as pg
 from settings_new import *
 from blocks_new import *
 import random, time
+from pygame.locals import *
 
 ###### NOTESSS #######
 #adjust WINDOWHEIGHT and WINDOWWIDTH constants, and BOARDHEIGHT and BOARDWIDTH 
@@ -15,7 +16,7 @@ import random, time
 
 #initialize pygame and set window
 pg.init()
-win = pg.display.set_mode((WIDTH, HEIGHT))
+win = pg.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pg.display.set_caption(GAME_NAME)
 Clock = pg.time.Clock()
 
@@ -25,9 +26,9 @@ Clock = pg.time.Clock()
 
 class Game():
     def __init__(self):
-        TITLE_FONT = pg.font.Font(pg.font.match_font("Comic Sans"), 100)
-        STD_FONT = pg.font.Font(pg.font.match_font("Comic Sans"), 20)
-        running = True
+        self.TITLE_FONT = pg.font.Font(pg.font.match_font("Comic Sans"), 100)
+        self.STD_FONT = pg.font.Font(pg.font.match_font("Comic Sans"), 20)
+        self.running = True
             
                  
     def runGame(self):
@@ -41,8 +42,8 @@ class Game():
         score = 0
         level, fallFreq = calculateLevelAndFallFreq(score)
     
-        fallingPiece = getNewPiece()
-        nextPiece = getNewPiece()
+        fallingPiece = newPiece()
+        nextPiece = newPiece()
     
         while True: # game loop
             if fallingPiece == None:
@@ -54,79 +55,51 @@ class Game():
                 if not isValidPosition(board, fallingPiece):
                     return # can't fit a new piece on the board, so game over
     
-            checkForQuit()
-            for event in pygame.event.get(): # event handling loop
-                if event.type == KEYUP:
-                    if (event.key == K_p):
-                        # Pausing the game
-                        DISPLAYSURF.fill(BGCOLOR)
-                        pygame.mixer.music.stop()
-                        showTextScreen('Paused') # pause until a key press
-                        pygame.mixer.music.play(-1, 0.0)
-                        lastFallTime = time.time()
-                        lastMoveDownTime = time.time()
-                        lastMoveSidewaysTime = time.time()
-                    elif (event.key == K_LEFT or event.key == K_a):
-                        movingLeft = False
-                    elif (event.key == K_RIGHT or event.key == K_d):
-                        movingRight = False
-                    elif (event.key == K_DOWN or event.key == K_s):
-                        movingDown = False
-    
-                elif event.type == KEYDOWN:
-                    # moving the piece sideways
-                    if (event.key == K_LEFT or event.key == K_a) and isValidPosition(board, fallingPiece, adjX=-1):
-                        fallingPiece['x'] -= 1
-                        movingLeft = True
-                        movingRight = False
-                        lastMoveSidewaysTime = time.time()
-    
-                    elif (event.key == K_RIGHT or event.key == K_d) and isValidPosition(board, fallingPiece, adjX=1):
-                        fallingPiece['x'] += 1
-                        movingRight = True
-                        movingLeft = False
-                        lastMoveSidewaysTime = time.time()
-    
-                    # rotating the piece (if there is room to rotate)
-                    elif (event.key == K_UP or event.key == K_w):
-                        fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % len(PIECES[fallingPiece['shape']])
-                        if not isValidPosition(board, fallingPiece):
-                            fallingPiece['rotation'] = (fallingPiece['rotation'] - 1) % len(PIECES[fallingPiece['shape']])
-                    elif (event.key == K_q): # rotate the other direction
+            if self.checkForClose():
+                return # The wndow has been closed, so game over.
+            
+            KEY = pg.key.get_pressed()
+            
+            for event in pg.event.get(): # event handling loop
+                if KEY[pg.K_UP]:
+                    fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % len(PIECES[fallingPiece['shape']])
+                    if not isValidPosition(board, fallingPiece):
                         fallingPiece['rotation'] = (fallingPiece['rotation'] - 1) % len(PIECES[fallingPiece['shape']])
-                        if not isValidPosition(board, fallingPiece):
-                            fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % len(PIECES[fallingPiece['shape']])
-    
-                    # making the piece fall faster with the down key
-                    elif (event.key == K_DOWN or event.key == K_s):
-                        movingDown = True
-                        if isValidPosition(board, fallingPiece, adjY=1):
-                            fallingPiece['y'] += 1
-                        lastMoveDownTime = time.time()
-    
-                    # move the current piece all the way down
-                    elif event.key == K_SPACE:
-                        movingDown = False
-                        movingLeft = False
-                        movingRight = False
-                        for i in range(1, BOARDHEIGHT):
-                            if not isValidPosition(board, fallingPiece, adjY=i):
-                                break
-                        fallingPiece['y'] += i - 1
-    
-            # handle moving the piece because of user input
-            if (movingLeft or movingRight) and time.time() - lastMoveSidewaysTime > MOVESIDEWAYSFREQ:
-                if movingLeft and isValidPosition(board, fallingPiece, adjX=-1):
-                    fallingPiece['x'] -= 1
-                elif movingRight and isValidPosition(board, fallingPiece, adjX=1):
-                    fallingPiece['x'] += 1
-                lastMoveSidewaysTime = time.time()
-    
-            if movingDown and time.time() - lastMoveDownTime > MOVEDOWNFREQ and isValidPosition(board, fallingPiece, adjY=1):
-                fallingPiece['y'] += 1
-                lastMoveDownTime = time.time()
-    
-            # let the piece fall if it is time to fall
+                        
+                elif KEY[pg.K_LEFT] and time.time() - lastMoveSidewaysTime > MOVESIDEWAYSFREQ:
+                    if isValidPosition(board, fallingPiece, adjX=-1):
+                        fallingPiece['x'] -= 1
+                    lastMoveSidewaysTime = time.time()      
+                    
+                elif KEY[pg.K_RIGHT] and time.time() - lastMoveSidewaysTime > MOVESIDEWAYSFREQ:
+                    if isValidPosition(board, fallingPiece, adjX=1):
+                        fallingPiece['x'] += 1
+                    lastMoveSidewaysTime = time.time()    
+                    
+                elif KEY[pg.K_p]:
+                    DISPLAYSURF.fill(BGCOLOR)
+                    showTextScreen('Paused')
+                    lastFallTime = time.time()
+                    lastMoveDownTime = time.time()
+                    lastMoveSidewaysTime = time.time()
+                    
+                # making the piece fall faster with the down key
+                elif KEY[pg.K_DOWN] and time.time() - lastMoveDownTime > MOVEDOWNFREQ:
+                    if isValidPosition(board, fallingPiece, adjY=1):
+                        fallingPiece['y'] += 1
+                    lastMoveDownTime = time.time()
+                    
+                        
+                elif (event.key == K_p):
+                    # Pausing the game
+                    DISPLAYSURF.fill(BGCOLOR)
+                    pg.mixer.music.stop()
+                    showTextScreen('Paused') # pause until a key press
+                    pg.mixer.music.play(-1, 0.0)
+                    lastFallTime = time.time()
+                    lastMoveDownTime = time.time()
+                    lastMoveSidewaysTime = time.time()
+                                   
             if time.time() - lastFallTime > fallFreq:
                 # see if the piece has landed
                 if not isValidPosition(board, fallingPiece, adjY=1):
@@ -138,17 +111,22 @@ class Game():
                 else:
                     # piece did not land, just move the piece down
                     fallingPiece['y'] += 1
-                    lastFallTime = time.time()
+                    lastFallTime = time.time()            
+    
+               
+    
+                   
+
     
             # drawing everything on the screen
-            DISPLAYSURF.fill(BGCOLOR)
+            win.fill(BGCOLOR)
             drawBoard(board)
             drawStatus(score, level)
             drawNextPiece(nextPiece)
             if fallingPiece != None:
                 drawPiece(fallingPiece)
     
-            pygame.display.update()
+            pg.display.update()
             FPSCLOCK.tick(FPS)   
             
             
@@ -156,7 +134,19 @@ class Game():
         board = []
         for i in range(BOARDWIDTH):
             board.append(["."] * BOARDHEIGHT)
-        return board        
+        return board
+    
+    def getNewPiece(self):
+        # return a random new piece in a random rotation and color
+        allShapes = list(PIECES.keys())
+        newShape = random.choice(allShapes)
+        newColor = random.choice(range(0, len(COLORS)-1))
+        newPiece = {'shape': newshape,
+                    'rotation': 1,
+                    'x': int(BOARDWIDTH / 2) - int(TEMPLATEWIDTH / 2),
+                    'y': -2, # start it above the board (i.e. less than 0)
+                    'color': newColor}
+        return newPiece    
         
     
     def showScreen(self, text):
@@ -166,19 +156,19 @@ class Game():
         
             
         #titleSurf, titleRect = makeTextObjs(text, TITLE_FONT, TEXTCOLOR)
-        titleObj = TITLE_FONT.render(text, True, color)
+        titleObj = self.TITLE_FONT.render(text, True, WHITE)
         titleRect = titleObj.get_rect()             
         titleRect.center = (int(WINDOWWIDTH / 2) , int(WINDOWHEIGHT / 2) )
         win.blit(titleObj, titleRect)            
         
         # Draw the additional "Press a key to play." text.
-        bodyObj = STD_FONT.render("Press any key to continue", True, color)
+        bodyObj = self.STD_FONT.render("Press any key to continue", True, WHITE)
         bodyRect = bodyObj.get_rect()  
         bodyRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2) + 100)
         win.blit(bodyObj, bodyRect)
     
         while self.senseAnyKey() == False:
-            pygame.display.update()
+            pg.display.update()
             Clock.tick()   
             
     """
@@ -188,29 +178,43 @@ class Game():
     """
     
     def senseAnyKey(self):
-        for event in pygame.event.get([KEYDOWN, KEYUP]):
+        for event in pg.event.get([KEYDOWN, KEYUP]):
             if event.type == KEYDOWN:
                 return True
         return False
     
-    def isValidPosition(board, piece, adjX=0, adjY=0):
+    def isValidPosition(self, board, piece, adjX=0, adjY=0):
         # Return True if the piece is within the board and not colliding
         for x in range(TEMPLATEWIDTH):
             for y in range(TEMPLATEHEIGHT):
                 isAboveBoard = y + piece['y'] + adjY < 0
+                isOnBoard = x + piece['x'] + adjX >= 0 and x +piece['x'] + adjX < BOARDWIDTH and y + piece['y'] + adjY < BOARDHEIGHT
                 if isAboveBoard or PIECES[piece['shape']][piece['rotation']][y][x] == BLANK:
                     continue
-                if not isOnBoard(x + piece['x'] + adjX, y + piece['y'] + adjY):
+                if not isOnBoard:
                     return False
                 if board[x + piece['x'] + adjX][y + piece['y'] + adjY] != BLANK:
                     return False
         return True    
+    
+    def closeButtonPressed(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+                return True
+            
+    def addToBoard(self):
+        for x in range(TEMPLATEWIDTH):
+            for y in range(TEMPLATEHEIGHT):
+                if PIECES[piece['shape']][piece['rotation']][y][x] != BLANK:
+                    board[x + piece['x']][y + piece['y']] = piece['color']
         
         
 g = Game()
 g.showScreen('Start')
 while g.running:
-    CLock.tick(FPS)
+    Clock.tick(FPS)
     g.runGame()
     
-self.showScreen('Game Over') 
+self.showScreen('Game Over')
+pg.quit()
